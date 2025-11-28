e7d7d60a4be765cfbe3b0160ea312257701e2e23
Snehagowda02 <snehamramesh02@gmail.com>
2025-11-28 10:14:18 +0530

    intial commit react router

---
+++ reactrouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Footer from './componrnts/Footer/Footer'
+import Header from './componrnts/Header/Header'
+import Home from './componrnts/Home/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
+// can delete App also here cause we are using route
